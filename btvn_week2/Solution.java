ed7e17af44cddd71af78d5d5b73acdc10f400cd0
Hai Hoang <nhuhaihoang31032007@gmail.com>
2026-09-21 00:28:15 +0700

    1.4.12 w2

---
+++ btvn_week2/Solution.java
@@ -0,0 +1,39 @@
+package btvn_week2;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void Solution(int[] a, int[] b) {
+        int i = 0;
+        int j = 0;
+        int n = a.length;
+        while (i < n && j < n ) {
+            if (a[i] == b[j]) {
+                System.out.println(a[i]);
+                i++;
+                j++;
+            } else if (a[i] < b[j]) {
+                i++;
+            }
+            else {
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int[] a = new int[n];
+        for (int k = 0;k < n;k++) {
+            a[k] = scan.nextInt();
+        }
+
+        int[] b = new int[n];
+        for (int k =0;k <n;k++){
+            b[k] = scan.nextInt();
+        }
+        Solution(a,b);
+    }
+}
