80d35c1eaa6aed8727fb70fb3ac34f80a21fdc29
Hai Hoang <nhuhaihoang31032007@gmail.com>
2026-09-20 21:17:42 +0700

    làm lại bài 1 w1

--- btvn_week1/bai01/BugQuickFind.java
+++ btvn_week1/bai01/BugQuickFind.java
@@ -23,12 +23,20 @@ public class BugQuickFind {
     }
 
     public static void main(String[] args) {
-        int n = 3;
+        int n = 3;             //tạo mảng leader = [0, 1, 2]
         BugQuickFind a = new BugQuickFind(n);
-        a.union(1, 0);
-        a.union(0, 2);
+        a.union(1, 0);       //gộp 1 và 0
+                                   //i = 0: leader[0] (0) != 1 -> giữ nguyên.
+                                    //i = 1: leader[1] (1) == 1 -> đổi leader[1] thành 0.
+                                    //i = 2: leader[2] (2) != 1 -> giữ nguyên.
+                                    //=> leader = [0, 0, 2] (đúng)
+        a.union(0, 2);       //ộp 0 và 2
+                                    //i = 0: leader[0] (0) == leader[0] (0) -> Đổi leader[0] = 2.mảng biến thành 2 [2, 0, 2]
+                                    //i = 1: leader[1] đang bằng 0. khi kiểm tra điều kiện leader[1] == leader[0] cho kết quả False sẽ ko chạy hàm trong if
+                                    //i = 2: ko chạy hàm trong if
+                                    //Kết quả: leader = [2, 0, 2]. (Sai Vì đúng theo quickfind thì phải là [2, 2, 2] nhưng find(1) = 0 khác find(2) = 2).
         System.out.println(a.find(1));
         System.out.println(a.find(2));
-        System.out.println((a.find(1) == a.find(2)));
+        System.out.println((a.find(1) == a.find(2))); // check
     }
 }
